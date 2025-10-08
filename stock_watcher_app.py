0484139b32ff1d8cdf309b9ca1b7861f81b23972
Fitsum Hiruy <fitsumhiruy@gmail.com>
2025-10-07 20:56:29 -0700

    Update stock_watcher_app.py

--- stock_watcher_app.py
+++ stock_watcher_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-
-st.set_page_config(page_title="Hello Streamlit", layout="centered")
+st.set_page_config(page_title="Hello Streamlit")
 st.markdown("# ✅ Streamlit is running")
-st.write("If you can read this, the deploy is good.")
-st.write("Streamlit version:", st.__version__)
+st.write("If you can read this, deploy works.")
